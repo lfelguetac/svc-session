6ee99d0ac139d04d33970d01c951630e80c998b3
jordancabral <jorcabral@falabella.com.ar>
2022-08-02 10:28:25 -0300

    added more repository test

--- app/repositories/user-session_test.go
+++ app/repositories/user-session_test.go
@@ -18,7 +18,7 @@ func TestSetUserSession(t *testing.T) {
 	})
 	repositories.SetClient(client)
 
-
+	// TODO: create complete session object and validate fields
 	session := SessionData{
 		Token: "123",
 		RefreshToken: "123"}
@@ -32,5 +32,44 @@ func TestSetUserSession(t *testing.T) {
 	_, err := s.Get("42")
 	if err != nil {
 		t.Error("Session is not saved")
+	}
+}
+
+func TestGetUserSessions(t *testing.T) {
+
+	s := miniredis.RunT(t)
+	client := redis.NewClient(&redis.Options{
+		Addr: s.Addr(),
+	})
+	repositories.SetClient(client)
+
+	s.Set("42", "bla")
+	// TODO: set entire session object
+
+	_, err := repositories.GetUserSessions("42")
+	if err != nil {
+		t.Error("Session is not saved")
+	}
+}
+
+func TestDeleteUserSession(t *testing.T) {
+
+	s := miniredis.RunT(t)
+	client := redis.NewClient(&redis.Options{
+		Addr: s.Addr(),
+	})
+	repositories.SetClient(client)
+
+	s.Set("42", "bla")
+	// TODO: set entire session object
+
+	err := repositories.DeleteUserSessions("42")
+	if err != nil {
+		t.Error("Session is not saved")
+	}
+
+	_, err = s.Get("42")
+	if err == nil {
+		t.Error("Session is deleted")
 	}
 }
